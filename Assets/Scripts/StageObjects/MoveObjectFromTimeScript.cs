3ac65b9208594f24e83fd5c2eb198910d109883e
gatsuonrails <gatsuonrails@gmail.com>
2019-10-06 03:42:25 +0900

    MoveObjectFromTimeScript 修正

--- Assets/Scripts/StageObjects/MoveObjectFromTimeScript.cs
+++ Assets/Scripts/StageObjects/MoveObjectFromTimeScript.cs
@@ -11,15 +11,49 @@ public class MoveObjectFromTimeScript : MonoBehaviour {
   private Vector3 startPosition;
   [System.NonSerialized]
   public bool reseted = false;
-  Vector3 defaultPosition;
+  public Vector3 defaultPosition;
+  public bool constantSpeed = false;
+  public float constantSpeedTime = 3.0f;
 
   private void Start() {
+    transform.position = destination[destination.Length - 1];
+    //一定の速度で動かす場合の処理
+    if (constantSpeed) {
+      System.Array.Resize(ref time, destination.Length);
+      //合計距離を求める
+      float sumDistance = 0;
+      int c = 0;
+      foreach(Vector3 des in destination) {
+        if (c < destination.Length - 1) {
+          sumDistance += Vector3.Distance(destination[c], destination[c + 1]);
+        }
+        else {
+          sumDistance += Vector3.Distance(destination[c], destination[0]);
+        }
+        c++;
+      }
+      //各距離の合計距離に対する割合を求める
+      float d;
+      c = 0;
+      foreach(float t in time) {
+        if (c < destination.Length - 1) {
+          d = Vector3.Distance(destination[c], destination[c + 1]) / sumDistance;
+        }
+        else {
+          d = Vector3.Distance(destination[c], destination[0]) / sumDistance;
+        }
+        time[c] = constantSpeedTime * d;
+        //time[c] = d;
+        c++;
+      }
+    }
+    //初期化セット
     startPosition = transform.position;
     startTime = Time.timeSinceLevelLoad;
     defaultPosition = transform.position;
   }
 
-  void Update() {
+  void FixedUpdate() {
     if (reseted) {
       i = 0;
       transform.position = defaultPosition;
@@ -35,61 +69,28 @@ public class MoveObjectFromTimeScript : MonoBehaviour {
       //歩行処理
       float dx = Mathf.Abs(direction.x);
       float dy = Mathf.Abs(direction.y);
-      if (dx > 0.02 || dy > 0.02) {
-        float key_x = 0;
-        float key_y = 0;
-        if (direction.x > 0) {
-          key_x = 1.0f;
+      if (dx > 0.03 || dy > 0.03) {
+        int t = 0;
+        if (i == 0) {
+          t = time.Length - 1;
         }
-        else if (direction.x < 0) {
-          key_x = -1.0f;
+        else if (i > 0) {
+          t = i - 1;
         }
-        else {
-          key_x = 0;
-        }
-        if (direction.y > 0) {
-          key_y = 1.0f;
-        }
-        else if (direction.y < 0) {
-          key_y = -1.0f;
-        }
-        else {
-          key_y = 0;
-        }
-
-        //transform.rotation = Quaternion.Euler(0, 0, 0);
-        //transform.Translate(new Vector2(key_x * speed * 0.02f, key_y * speed * 0.02f));
-        //transform.rotation = Quaternion.Euler(0, 0, defaultAngleZ);
-
         var diff = Time.timeSinceLevelLoad - startTime;
-        if (diff > time[i]) {
-          transform.position = destination[i];
-        }
 
-        var rate = diff / time[i];
-        //var pos = curve.Evaluate(rate);
-
+        var rate = diff / time[t];
         transform.position = Vector3.Lerp(startPosition, destination[i], rate);
 
-        //目的地通り過ぎた時に目的地まで戻す
-        if (direction.x > 0) {
-          if (destination[i].x < transform.position.x) {
-            transform.position = new Vector2(destination[i].x, transform.position.y);
+        if (diff >= time[t]) {
+          transform.position = destination[i];
+          startTime = Time.timeSinceLevelLoad;
+          startPosition = transform.position;
+          if (i < destination.Length) {
+            i++;
           }
-        }
-        else if (direction.x < 0) {
-          if (destination[i].x > transform.position.x) {
-            transform.position = new Vector2(destination[i].x, transform.position.y);
-          }
-        }
-        if (direction.y > 0) {
-          if (destination[i].y < transform.position.y) {
-            transform.position = new Vector2(transform.position.x, destination[i].y);
-          }
-        }
-        else if (direction.y < 0) {
-          if (destination[i].y > transform.position.y) {
-            transform.position = new Vector2(transform.position.x, destination[i].y);
+          else {
+            i = 0;
           }
         }
       }
