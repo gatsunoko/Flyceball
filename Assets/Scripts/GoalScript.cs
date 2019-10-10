1f7576c8f9ac092418a8899f2a3be559dc4ea6fa
gatsuonrails <gatsuonrails@gmail.com>
2019-10-11 08:51:12 +0900

    クリアしたステージの色かわるようにした

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -6,6 +6,8 @@ public class GoalScript : MonoBehaviour {
 
   AudioSource goalSound;
   StageDirectorScript stageDirectorScript;
+  public string thisStage = "1";
+  public string nextStage = "4";
 
   void Start() {
     this.goalSound = GetComponent<AudioSource>();
@@ -16,6 +18,10 @@ public class GoalScript : MonoBehaviour {
     if (col.gameObject.tag == "Player" && !this.stageDirectorScript.clear) {
       this.goalSound.PlayOneShot(goalSound.clip);
       this.stageDirectorScript.clear = true;
+      PlayerPrefs.SetInt("Stage" + thisStage, 2);
+      if (PlayerPrefs.GetInt("Stage" + nextStage) != 2) {
+        PlayerPrefs.SetInt("Stage" + nextStage, 1);
+      }
     }
   }
 }
