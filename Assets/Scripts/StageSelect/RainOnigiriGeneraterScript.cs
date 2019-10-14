61ec96707eca7f56dec9791ef421390e459fdfac
gatsuonrails <gatsuonrails@gmail.com>
2019-10-14 22:33:09 +0900

    ImageChange

---
+++ Assets/Scripts/StageSelect/RainOnigiriGeneraterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainOnigiriGeneraterScript : MonoBehaviour {
+
+  public GameObject onigiri;
+  float x;
+  float time = 0;
+
+  void Start() {
+
+  }
+
+  void Update() {
+    time += Time.deltaTime;
+    if (time >= 0.3f) {
+      time = 0;
+      GameObject onigiriInstance = Instantiate(onigiri) as GameObject;
+      x = Random.Range(2.43f, 18.59f);
+      onigiriInstance.transform.position = new Vector2(x, 3.54f);
+    }
+  }
+}
---
+++ Assets/Scripts/StageSelect/RainOnigiriScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainOnigiriScript : MonoBehaviour {
+
+  void Update() {
+    if (transform.position.y <= -10.10497f) {
+      Destroy(gameObject);
+    }
+  }
+}
