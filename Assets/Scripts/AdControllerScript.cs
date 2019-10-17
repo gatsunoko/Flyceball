094878f5d4e030bbd9e34321fdf9233b127a4203
gatsuonrails <gatsuonrails@gmail.com>
2019-10-17 20:10:54 +0900

    広告追加

---
+++ Assets/Scripts/AdControllerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdControllerScript : SingletonMonoBehaviourFast<AdControllerScript> {
+
+  public float adIntervalTime = 0;
+
+  void Start() {
+    DontDestroyOnLoad(this);
+  }
+
+  void Update() {
+    this.adIntervalTime += Time.deltaTime;
+  }
+}
