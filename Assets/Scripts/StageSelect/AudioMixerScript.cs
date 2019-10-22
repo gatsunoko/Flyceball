7c44b0caa1d7e91adb04bfff370bf29f05c10983
gatsuonrails <gatsuonrails@gmail.com>
2019-10-22 21:20:14 +0900

    AudioMixer Added

---
+++ Assets/Scripts/StageSelect/AudioMixerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioMixerScript : MonoBehaviour {
+
+  //　SoundOptionキャンバスを設定
+  [SerializeField]
+  private GameObject soundOptionUI;
+  [SerializeField]
+  private AudioMixer audioMixer;
+  [SerializeField]
+  private GameObject sliderObject;
+  private Slider slider;
+
+  private void Start() {
+    soundOptionUI.SetActive(false);
+    slider = sliderObject.GetComponent<Slider>();
+    if (PlayerPrefs.HasKey("AudioVolume")) {
+      slider.value = PlayerPrefs.GetFloat("AudioVolume");
+    }
+    else {
+      slider.value = 0;
+    }
+  }
+
+  public void SetMaster(float volume) {
+    audioMixer.SetFloat("Master", slider.value);
+    PlayerPrefs.SetFloat("AudioVolume", slider.value);
+  }
+}
---
+++ Assets/Scripts/StageSelect/OptionButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionButtonScript : MonoBehaviour {
+
+  Vector3 startPosition;
+  [SerializeField]
+  GameObject optionUI;
+
+  public void OnClickStart() {
+    startPosition = Input.mousePosition;
+  }
+
+  public void OnClickEnd() {
+    Vector3 distance = startPosition - Input.mousePosition;
+    float distanceX = Mathf.Abs(distance.x);
+    float distanceY = Mathf.Abs(distance.y);
+
+    if (distanceX < 20.0f && distanceY < 20.0f) {
+      if (!optionUI.activeSelf) {
+        optionUI.SetActive(true);
+      }
+      else {
+        optionUI.SetActive(false);
+      }
+    }
+  }
+}
