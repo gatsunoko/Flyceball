4ee51d28ee86057f67b6e3f6c94be52b3cfca88a
gatsuonrails <gatsuonrails@gmail.com>
2019-10-23 11:29:43 +0900

    conflyct fixed

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Advertisements;
 
 public class GameControllerScript : MonoBehaviour {
 
   PlayerScript playerScript;
   float deathAfterTime = 0;
   public bool saveEnabled = false;
-  float adBeforeTime = 0;
 
   void Start() {
     this.playerScript = PlayerScript.playerScript;
@@ -17,28 +15,13 @@ public class GameControllerScript : MonoBehaviour {
 
   void Update() {
     if (this.playerScript.dead) {
-      if (Advertisement.IsReady() && AdControllerScript.Instance.adIntervalTime >= 600.0f) {
-        this.adBeforeTime += Time.deltaTime;
-        if (this.adBeforeTime >= 1.0f) {
-          if (GameObject.Find("StageUI")) {
-            GameObject.Find("StageUI").gameObject.SetActive(false);
-          }
-          AdControllerScript.Instance.adIntervalTime = 0;
-          Advertisement.Show();
-          return;
-        }
-        return;
+      this.deathAfterTime += Time.deltaTime;
+      if (this.deathAfterTime >= 2.5f) {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
       }
-
-      if (!Advertisement.isShowing) {
-        this.deathAfterTime += Time.deltaTime;
-        if (this.deathAfterTime >= 2.5f) {
-          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        //画面クリックしたらリセット
-        if (Input.GetMouseButtonUp(0)) {
-          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+      //画面クリックしたらリセット
+      if (Input.GetMouseButtonUp(0)) {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
       }
     }
   }
